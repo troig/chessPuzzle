55b37b18c141e6679aff81dea4ebcf6a601da929
troig <tomeuroig@gmail.com>
2014-09-17 22:30:33 +0200

    Skeleton for module chessPuzzle-front: Basic webapp to the solve chess puzzle (spring-mvc + jsp based running over tomcat)

---
+++ chessPuzzle-front/src/main/java/uk/tryCatch/javaDeveloper/front/controller/IndexController.java
@@ -0,0 +1,21 @@
+package uk.tryCatch.javaDeveloper.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for the index.
+ *
+ * @author troig
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+   @RequestMapping(method = RequestMethod.GET)
+   public ModelAndView index(){
+      return new ModelAndView("index");
+   }
+}
---
+++ chessPuzzle-front/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,13 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ChessPuzzle</title>
+
+</head>
+<body>
+Index
+</body>
+</html>
